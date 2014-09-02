6fbe769eab9f261b836dce22ba3a72de9e31526c
Poly Takahiro Horikawa <horikawa.takahiro@gmail.com>
2014-09-02 02:30:07 -0700

    Add fetching image data

--- src/com/polysfactory/mirrorapisample/servlet/SubscriptionCallbackServlet.java
+++ src/com/polysfactory/mirrorapisample/servlet/SubscriptionCallbackServlet.java
@@ -1,6 +1,8 @@
 package com.polysfactory.mirrorapisample.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -9,12 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpResponse;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.mirror.Mirror;
+import com.google.api.services.mirror.Mirror.Timeline.Attachments.Get;
+import com.google.api.services.mirror.model.Attachment;
 import com.google.api.services.mirror.model.Notification;
+import com.google.api.services.mirror.model.NotificationConfig;
 import com.google.api.services.mirror.model.TimelineItem;
+import com.google.api.services.mirror.model.UserAction;
+import com.google.common.io.ByteStreams;
 import com.polysfactory.mirrorapisample.util.AuthUtil;
 import com.polysfactory.mirrorapisample.util.MirrorUtil;
+import com.polysfactory.mirrorapisample.util.WebUtil;
 
 @SuppressWarnings("serial")
 public class SubscriptionCallbackServlet extends HttpServlet {
@@ -37,6 +48,26 @@ public class SubscriptionCallbackServlet extends HttpServlet {
             String timelineItemId = notification.getItemId();
             TimelineItem timelineItem = mirror.timeline().get(timelineItemId).execute();
             LOGGER.info(timelineItem.toPrettyString());
+
+            if (notification.getUserActions().contains(new UserAction().setType("SHARE"))
+                    && timelineItem.getAttachments() != null
+                    && timelineItem.getAttachments().size() > 0) {
+                for (Attachment attachment : timelineItem.getAttachments()) {
+                    HttpResponse response = mirror.getRequestFactory()
+                            .buildGetRequest(new GenericUrl(attachment.getContentUrl())).execute();
+                    InputStream is = response.getContent();
+                    byte[] bytes = ByteStreams.toByteArray(is);
+
+                    // 受け取った画像で新たにタイムラインを作成
+                    TimelineItem newTimelineItem = new TimelineItem();
+                    newTimelineItem.setText("画像を受け取りました");
+                    newTimelineItem.setNotification(new NotificationConfig().setLevel("DEFAULT"));
+
+                    Mirror.Timeline timeline = mirror.timeline();
+                    timeline.insert(newTimelineItem, new ByteArrayContent("image/jpeg", bytes))
+                            .execute();
+                }
+            }
         }
         resp.getWriter().println("OK");
     }
